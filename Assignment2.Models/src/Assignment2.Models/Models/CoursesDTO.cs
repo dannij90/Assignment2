1f2d8935913099859a16df33f6c7f51744957d56
DESKTOP-5UJO18F\dannij90 <dannij90@gmail.com>
2016-08-31 17:54:46 +0000

    .

---
+++ Assignment2.Models/src/Assignment2.Models/Models/CoursesDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models.Models
+{
+    public class CoursesDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Semester { get; set; }
+        public string TemplateID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
+
